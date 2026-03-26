8813e81788d1f9b0b44cefc91ad008b5f7667f13
jeevansandeep7 <jeevansandeep2007@gmail.com>
2026-03-26 18:36:07 +0000

    progress update

--- dist-newstyle/build/x86_64-linux/ghc-9.4.7/haskell-0.1.0.0/x/haskell/build/haskell/autogen/cabal_macros.h
+++ dist-newstyle/build/x86_64-linux/ghc-9.4.7/haskell-0.1.0.0/x/haskell/build/haskell/autogen/cabal_macros.h
@@ -40,6 +40,16 @@
   (major1) == 0 && (major2) <  5 || \
   (major1) == 0 && (major2) == 5 && (minor) <= 4)
 #endif /* MIN_VERSION_cassava */
+/* package directory-1.3.7.1 */
+#ifndef VERSION_directory
+#define VERSION_directory "1.3.7.1"
+#endif /* VERSION_directory */
+#ifndef MIN_VERSION_directory
+#define MIN_VERSION_directory(major1,major2,minor) (\
+  (major1) <  1 || \
+  (major1) == 1 && (major2) <  3 || \
+  (major1) == 1 && (major2) == 3 && (minor) <= 7)
+#endif /* MIN_VERSION_directory */
 /* package scotty-0.30 */
 #ifndef VERSION_scotty
 #define VERSION_scotty "0.30"
